a12ee06896e804bd00cc3d89d5ab964e49e07e42
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2018-11-21 19:27:20 +0400

    Updated

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ var t1 = new fabric.Textbox('MyText', {
     left: 5,
     fontSize: 50,
     textAlign: 'center',
-    fixedWidth: 150
+    // fixedWidth: 150
 });
 
 canvas.on('text:changed', function(opt) {
@@ -30,3 +30,23 @@ canvas.on('text:changed', function(opt) {
 });
 
 canvas.add(t1);
+
+document.getElementById('image').addEventListener("change", function(e) {
+   var file = e.target.files[0];
+   var reader = new FileReader();
+   reader.onload = function(f) {
+      var data = f.target.result;
+      fabric.Image.fromURL(data, function(img) {
+         // add background image
+         canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+            scaleX: canvas.width / img.width,
+            scaleY: canvas.height / img.height
+         });
+      });
+   };
+   reader.readAsDataURL(file);
+});
+
+window.onclick = () => {
+  console.log(canvas);
+};
