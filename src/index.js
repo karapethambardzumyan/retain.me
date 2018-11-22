782aa802051fee995b87c09564b4036d5ba9c91c
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2018-11-22 18:02:30 +0400

    Structuring

--- src/index.js
+++ src/index.js
@@ -1,53 +1,7 @@
-import { fabric } from 'fabric';
 import main from './main';
 
-const sizes = [
-  { width: 500, height: 500 },
-  { width: 400, height: 400 },
-  { width: 300, height: 300 },
-  { width: 200, height: 200 }
-];
-
-let a = null;
-
-main.run((config) => {
-  const canvas = new fabric.Canvas('canvas');
-
-  if(config.canvas.background !== null) {
-    fabric.Image.fromURL(config.canvas.background, function(img) {
-      canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
-        scaleX: canvas.width / img.width,
-        scaleY: canvas.height / img.height
-      });
-    });
-  }
-
-  document.getElementById('upload-background').onchange = e => {
-    let file = e.target.files[0];
-    let reader = new FileReader();
-
-    reader.onload = f => {
-      let data = f.target.result;
-
-      fabric.Image.fromURL(data, function(img) {
-        canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
-          scaleX: canvas.width / img.width,
-          scaleY: canvas.height / img.height
-        });
-      });
-    };
-
-    reader.readAsDataURL(file);
-  };
-
-  document.getElementById('save').onclick = e => {
-    const background = canvas.backgroundImage.getSrc();
-
-    localStorage.setItem('config', JSON.stringify({
-      canvas: {
-        ...config.canvas,
-        background: background
-      }
-    }));
-  };
-});
+main
+  .init()
+  .uploadBackground()
+  .reset()
+  .save();
--- src/main.js
+++ src/main.js
@@ -1,28 +1,104 @@
+import { fabric } from 'fabric';
+
 const config = {
-  canvas: {
-    background: null,
-    size: 0
-  }
+  background: null,
+  size: 0,
+  text: []
 };
+
+const sizes = [
+  { width: 500, height: 500 },
+  { width: 400, height: 400 },
+  { width: 300, height: 300 },
+  { width: 200, height: 200 }
+];
 
 class Main {
   constructor() {
+    this.config = this.initialConfig();
+    this.canvas = null;
+  };
+
+  init() {
+    this.canvas = new fabric.Canvas('canvas');
+
+    this.canvas.setWidth(sizes[this.config.size].width);
+    this.canvas.setHeight(sizes[this.config.size].height);
+
+    if(this.config.background !== null) {
+      fabric.Image.fromURL(this.config.background, img => {
+        this.canvas.setBackgroundImage(img, this.canvas.renderAll.bind(this.canvas), {
+          scaleX: this.canvas.width / img.width,
+          scaleY: this.canvas.height / img.height
+        });
+      });
+    }
+
+    return this;
+  };
+
+  uploadBackground() {
+    document.getElementById('upload-background').onchange = e => {
+      let file = e.target.files[0];
+      let reader = new FileReader();
+
+      reader.onload = f => {
+        let data = f.target.result;
+
+        console.log('Background is finished uploading');
+        fabric.Image.fromURL(data, img => {
+          this.canvas.setBackgroundImage(img, this.canvas.renderAll.bind(this.canvas), {
+            scaleX: this.canvas.width / img.width,
+            scaleY: this.canvas.height / img.height
+          });
+        });
+      };
+
+      console.log('Background is started uploading');
+      reader.readAsDataURL(file);
+    };
+
+    return this;
+  };
+
+  reset() {
+    document.getElementById('reset').onclick = e => {
+      this.config = config;
+      this.canvas.clear();
+
+      localStorage.setItem('config', JSON.stringify(config));
+
+      alert('Reseted!');
+    };
+
+    return this;
+  };
+
+  save() {
+    document.getElementById('save').onclick = e => {
+      if(this.canvas.backgroundImage === null) {
+        return alert('There is nothing to be saved!');
+      } else {
+        const background = this.canvas.backgroundImage.getSrc();
+
+        localStorage.setItem('config', JSON.stringify({
+          ...this.config,
+          background
+        }));
+
+        alert('State is saved!');
+      }
+    };
+
+    return this;
+  };
+
+  initialConfig() {
     if(localStorage.getItem('config') === null) {
       localStorage.setItem('config', JSON.stringify(config));
     }
 
-    this.config = JSON.parse(localStorage.getItem('config'));
-    this.ratio = window.devicePixelRatio;
-    this.width = 400 * this.ratio;
-    this.height = 400 * this.ratio;
-  };
-
-  run(cb) {
-    this.canvas = document.getElementById('canvas');
-    this.canvas.width = this.width;
-    this.canvas.height = this.height;
-
-    return cb(this.config);
+    return JSON.parse(localStorage.getItem('config'));
   };
 };
 
