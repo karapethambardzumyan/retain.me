834c36a5fc9336b75af468a7fc49cb14bf46598f
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2018-11-25 16:15:32 +0400

    Found out the scale formula))

--- src/index.js
+++ src/index.js
@@ -20,6 +20,58 @@ main.init(canvas => {
     }
   });
 
+  canvas.on('object:scaling', options => {
+    const innerCanvas = {
+      width: main.canvas.width * 0.8,
+      height: main.canvas.height * 0.8
+    };
+    const outerCanvas = {
+      width: main.canvas.width,
+      height: main.canvas.height
+    };
+    const offset = {
+      left: (outerCanvas.width - innerCanvas.width) / 2,
+      top: (outerCanvas.height - innerCanvas.height) / 2
+    };
+
+    let target = options.target;
+    let grid = 50;
+    let w = target.getScaledWidth();
+    let h = target.getScaledHeight();
+    let snap = {
+       top: Math.round(target.top / grid) * grid,
+       left: Math.round(target.left / grid) * grid,
+       bottom: Math.round((target.top + h) / grid) * grid,
+       right: Math.round((target.left + w) / grid) * grid
+    };
+    let threshold = grid;
+    let dist = {
+      top: Math.abs(snap.top - target.top),
+      left: Math.abs(snap.left - target.left),
+      bottom: Math.abs(snap.bottom - target.top - h),
+      right: Math.abs(snap.right - target.left - w)
+    };
+    let attrs = {
+      scaleX: target.scaleX,
+      scaleY: target.scaleY,
+      top: target.top,
+      left: target.left
+    };
+
+    switch(target.__corner) {
+      case 'ml':
+        if(target.left - offset.left < 14) {
+          attrs.scaleX = (w - (snap.left - target.left)) / target.width;
+          attrs.left = snap.left;
+        }
+        break;
+      default:
+        break;
+    }
+
+    target.set(attrs);
+  });
+
   canvas.on('object:moving', e => {
     if(e.target !== null && canvas.getActiveObject() && canvas.getActiveObject().get('type') === 'image') {
       const innerCanvas = {
