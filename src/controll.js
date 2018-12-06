47720043cc9060eed01ffd1af2157c0d1fdd0e1f
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2018-12-06 20:56:12 +0400

    Updated

--- src/controll.js
+++ src/controll.js
@@ -261,7 +261,7 @@ class Controll {
     };
 
     document.getElementById('font-color-value').onkeyup = e => {
-      document.getElementById('font-color').value = e.target.value;
+      document.getElementById('font-color-value').value = e.target.value;
       document.getElementById('font-color-preview').style.background = e.target.value;
 
       text.setColor(e.target.value);
