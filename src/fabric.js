6bd44049344aca80301eec55dca018937025e8c3
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2019-01-13 00:38:06 +0400

    Added backspace and delete support for rt;

--- src/fabric.js
+++ src/fabric.js
@@ -2,6 +2,60 @@ import { fabric } from 'fabric';
 
 fabric.IText.prototype.isRTL = true;
 
+fabric.IText.prototype.onKeyDown = function(e) {
+  if(!this.isEditing || this.inCompositionMode) {
+    return;
+  }
+  if(e.keyCode === 8) {
+    e.preventDefault();
+
+    if(this.selectionStart === this.text.length) {
+      return;
+    }
+
+    let value = this.hiddenTextarea.value.split('');
+    value.splice(this.selectionStart, this.selectionStart === this.selectionEnd ? 1 : Math.abs(this.selectionStart - this.selectionEnd));
+    value = value.join('');
+
+    this.selectionEnd = this.selectionStart;
+    this.hiddenTextarea.value = value;
+    this.text = this.hiddenTextarea.value;
+  }
+  if(e.keyCode === 46) {
+    e.preventDefault();
+
+    if(this.selectionStart === 0) {
+      return;
+    }
+
+    let value = this.hiddenTextarea.value.split('');
+    value.splice(this.selectionStart - 1, this.selectionStart === this.selectionEnd ? 1 : Math.abs(this.selectionStart - this.selectionEnd));
+    value = value.join('');
+
+    this.selectionEnd = this.selectionStart = this.selectionStart - 1;
+    this.hiddenTextarea.value = value;
+    this.text = this.hiddenTextarea.value;
+  }
+  if(e.keyCode in this.keysMap) {
+    this[this.keysMap[e.keyCode]](e);
+  }
+  else if ((e.keyCode in this.ctrlKeysMapDown) && (e.ctrlKey || e.metaKey)) {
+    this[this.ctrlKeysMapDown[e.keyCode]](e);
+  }
+  else {
+    return;
+  }
+  e.stopImmediatePropagation();
+  e.preventDefault();
+  if(e.keyCode >= 33 && e.keyCode <= 40) {
+    this.clearContextTop();
+    this.renderCursorOrSelection();
+  }
+  else {
+    this.canvas && this.canvas.requestRenderAll();
+  }
+},
+
 fabric.IText.prototype._renderChars = function(method, ctx, line, left, top, lineIndex) {
   let lineHeight = this.getHeightOfLine(lineIndex);
   let charBox;
@@ -24,24 +78,27 @@ fabric.IText.prototype.updateFromTextArea = function() {
     return;
   }
 
-  const lastChar = getDifference(this.text, this.hiddenTextarea.value);
-  const index = this.hiddenTextarea.value.indexOf(lastChar);
+  this.hiddenTextarea.selectionStart = this.hiddenTextarea.selectionStart - 1;
+  this.hiddenTextarea.selectionEnd = this.hiddenTextarea.selectionEnd - 1;
 
-  if(isNumber(this.hiddenTextarea.value[index]) && isNumber(this.hiddenTextarea.value[index + 1])) {
-    let str = this.hiddenTextarea.value;
-    str = str.split('');
-    str.splice(index, 1);
-    str.splice(index + 1, 0, lastChar);
-    this.hiddenTextarea.value = str.join('');
-    this.hiddenTextarea.selectionStart = index + 1;
-    this.hiddenTextarea.selectionEnd = index + 1;
-  }
+  // const lastChar = getDifference(this.text, this.hiddenTextarea.value);
+  // const index = this.hiddenTextarea.value.indexOf(lastChar);
+  //
+  // if(isNumber(this.hiddenTextarea.value[index]) && isNumber(this.hiddenTextarea.value[index + 1])) {
+  //   let str = this.hiddenTextarea.value;
+  //   // let newCharIndex = str.slice(index + 1);
+  //   // console.log(newCharIndex);
+  //
+  //   str = str.split('');
+  //   str.splice(index, 1);
+  //   str.splice(index + 1, 0, lastChar);
+  //   this.hiddenTextarea.value = str.join('');
+  //   this.hiddenTextarea.selectionStart = index + 1;
+  //   this.hiddenTextarea.selectionEnd = index + 1;
+  // }
 
   this.cursorOffsetCache = { };
   this.text = this.hiddenTextarea.value;
-
-  this.hiddenTextarea.selectionStart = this.hiddenTextarea.selectionStart - 1;
-  this.hiddenTextarea.selectionEnd = this.hiddenTextarea.selectionEnd - 1;
 
   if(this._shouldClearDimensionCache()) {
     this.initDimensions();
@@ -55,26 +112,26 @@ fabric.IText.prototype.updateFromTextArea = function() {
   this.updateTextareaPosition();
 };
 
-function getDifference(a, b) {
-    let i = 0;
-    let j = 0;
-    let result = '';
-
-    while (j < b.length) {
-      if(a[i] != b[j] || i == a.length) {
-          result += b[j];
-      } else {
-        i++;
-      }
-
-      j++;
-    }
-
-    return result;
-};
-
-function isNumber(str) {
-  str = parseInt(str);
-
-  return (typeof str === 'number' && !isNaN(str));
-};
+// function getDifference(a, b) {
+//     let i = 0;
+//     let j = 0;
+//     let result = '';
+//
+//     while (j < b.length) {
+//       if(a[i] != b[j] || i == a.length) {
+//           result += b[j];
+//       } else {
+//         i++;
+//       }
+//
+//       j++;
+//     }
+//
+//     return result;
+// };
+//
+// function isNumber(str) {
+//   str = parseInt(str);
+//
+//   return (typeof str === 'number' && !isNaN(str));
+// };
