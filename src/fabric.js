45206170207931aed75e7258dad5dedfe77a2c08
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2019-01-13 19:40:26 +0400

    Updated

--- src/fabric.js
+++ src/fabric.js
@@ -87,6 +87,31 @@ fabric.IText.prototype._renderChars = function(method, ctx, line, left, top, lin
   ctx.restore();
 };
 
+fabric.IText.prototype._renderChar = function(method, ctx, lineIndex, charIndex, _char, left, top) {
+  var decl = this._getStyleDeclaration(lineIndex, charIndex),
+      fullDecl = this.getCompleteStyleDeclaration(lineIndex, charIndex),
+      shouldFill = method === 'fillText' && fullDecl.fill,
+      shouldStroke = method === 'strokeText' && fullDecl.stroke && fullDecl.strokeWidth;
+
+  if (!shouldStroke && !shouldFill) {
+    return;
+  }
+  decl && ctx.save();
+
+  this._applyCharStyles(method, ctx, lineIndex, charIndex, fullDecl);
+
+  if (decl && decl.textBackgroundColor) {
+    this._removeShadow(ctx);
+  }
+  if (decl && decl.deltaY) {
+    top += decl.deltaY;
+  }
+
+  shouldFill && ctx.fillText(_char, left, top);
+  shouldStroke && ctx.strokeText(_char, left, top);
+  decl && ctx.restore();
+};
+
 fabric.IText.prototype.updateFromTextArea = function() {
   if(!this.hiddenTextarea) {
     return;
@@ -100,18 +125,18 @@ fabric.IText.prototype.updateFromTextArea = function() {
   let index = text.indexOf(lastChar);
 
   // section with numbers direction is wrong, should be refactored
-  // if(isNumber(lastChar) && isNumber(text[index + 1])) {
-  //   let closestNanIndex = text.match(/[^0-9]/);
-  //   closestNanIndex = closestNanIndex && closestNanIndex.index - 1;
-  //   text = text.split('');
-  //   text.splice(index, 1);
-  //   text.splice(closestNanIndex, 0, lastChar);
-  //   text = text.join('');
-  //
-  //   this.hiddenTextarea.value = text;
-  //   this.hiddenTextarea.selectionStart = index;
-  //   this.hiddenTextarea.selectionEnd = index;
-  // }
+  if(isNumber(lastChar) && isNumber(text[index + 1])) {
+    let closestNanIndex = text.match(/[^0-9]/);
+    closestNanIndex = closestNanIndex && closestNanIndex.index - 1;
+    text = text.split('');
+    text.splice(index, 1);
+    text.splice(closestNanIndex, 0, lastChar);
+    text = text.join('');
+
+    this.hiddenTextarea.value = text;
+    this.hiddenTextarea.selectionStart = index;
+    this.hiddenTextarea.selectionEnd = index;
+  }
 
   this.cursorOffsetCache = { };
   this.text = this.hiddenTextarea.value;
@@ -126,6 +151,74 @@ fabric.IText.prototype.updateFromTextArea = function() {
     this.selectionStart = newSelection.selectionStart;
   }
   this.updateTextareaPosition();
+};
+
+fabric.IText.prototype.renderCursor = function(boundaries, ctx) {
+  var cursorLocation = this.get2DCursorLocation(),
+      lineIndex = cursorLocation.lineIndex,
+      charIndex = cursorLocation.charIndex > 0 ? cursorLocation.charIndex - 1 : 0,
+      charHeight = this.getValueOfPropertyAt(lineIndex, charIndex, 'fontSize'),
+      multiplier = this.scaleX * this.canvas.getZoom(),
+      cursorWidth = this.cursorWidth / multiplier,
+      topOffset = boundaries.topOffset,
+      dy = this.getValueOfPropertyAt(lineIndex, charIndex, 'deltaY');
+
+  topOffset += (1 - this._fontSizeFraction) * this.getHeightOfLine(lineIndex) / this.lineHeight - charHeight * (1 - this._fontSizeFraction);
+
+  if (this.inCompositionMode) {
+    this.renderSelection(boundaries, ctx);
+  }
+
+  ctx.fillStyle = this.getValueOfPropertyAt(lineIndex, charIndex + 1, 'fill');
+  ctx.globalAlpha = this.__isMousedown ? 1 : this._currentCursorOpacity;
+  ctx.fillRect(
+    boundaries.left + boundaries.leftOffset - cursorWidth / 2,
+    topOffset + boundaries.top + dy,
+    cursorWidth,
+    charHeight);
+};
+
+fabric.IText.prototype.insertCharStyleObject = function(lineIndex, charIndex, quantity, copiedStyle) {
+  if (!this.styles) {
+    this.styles = {};
+  }
+  var currentLineStyles       = this.styles[lineIndex],
+      currentLineStylesCloned = currentLineStyles ? fabric.util.object.clone(currentLineStyles) : {};
+
+  quantity || (quantity = 1);
+  // shift all char styles by quantity forward
+  // 0,1,2,3 -> (charIndex=2) -> 0,1,3,4 -> (insert 2) -> 0,1,2,3,4
+  for (var index in currentLineStylesCloned) {
+    var numericIndex = parseInt(index, 10);
+    if (numericIndex >= charIndex) {
+      currentLineStyles[numericIndex + quantity] = currentLineStylesCloned[numericIndex];
+      // only delete the style if there was nothing moved there
+      if (!currentLineStylesCloned[numericIndex - quantity]) {
+        delete currentLineStyles[numericIndex];
+      }
+    }
+  }
+  this._forceClearCache = true;
+  if (copiedStyle) {
+    while (quantity--) {
+      if (!Object.keys(copiedStyle[quantity]).length) {
+        continue;
+      }
+      if (!this.styles[lineIndex]) {
+        this.styles[lineIndex] = {};
+      }
+      this.styles[lineIndex][charIndex + quantity] = fabric.util.object.clone(copiedStyle[quantity]);
+    }
+    return;
+  }
+  if (!currentLineStyles) {
+    return;
+  }
+  var newStyle = currentLineStyles[charIndex ? charIndex + 1 : 1];
+  console.log(newStyle);
+  while (newStyle && quantity--) {
+    this.styles[lineIndex][charIndex + quantity] = fabric.util.object.clone(newStyle);
+  }
 };
 
 // helpers
